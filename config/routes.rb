15210b889a362ff7a1d388b6c2cc648659b414ec
sgibson47 <sgibson47@gmail.com>
2018-09-06 12:35:48 -0500

    add sessions#new, sessions#create, and sessions#destroy actions as well as routes for same

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
 
   resources :recipes
+
+  get '/signin', to: 'sessions#new'
+
+  post '/signin', to: 'sessions#create'
+
+  delete "/signout", to: "sessions#destroy"
 end
